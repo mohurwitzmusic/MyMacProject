c88b603331c3da562ed6ee2f0fe765697895157b
MIDIKinetics <moh@midikinetics.com>
2020-07-12 17:41:19 -0700

    Initial Commit

---
+++ MyMacProject/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyMacProject
+//
+//  Created by Michael O. Hurwitz on 2020-07-12.
+//  Copyright © 2020 MIDI Kinetics. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MyMacProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyMacProject
+//
+//  Created by Michael O. Hurwitz on 2020-07-12.
+//  Copyright © 2020 MIDI Kinetics. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
